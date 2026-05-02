e220b1fcbf1cbdc4cb2cf41a38dfe2173d647832
Anoop K N <anoopkn2001@pm.me>
2026-05-02 18:52:07 +0530

    feat: First commit

---
+++ src/test/java/com/anoopkn/ticketsystem/TicketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoopkn.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
